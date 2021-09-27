2796229d1999630fe8fe40433df978550c2abb17
Admin <luis.pablo.peralta.tapia@gmail.com>
2021-09-26 19:54:32 -0500

    funciones de cambio de ventana

---
+++ src/Controller/RegisterController.java
@@ -0,0 +1,79 @@
+
+package Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RegisterController implements Initializable {
+
+    @FXML
+    private Button btnIniciarSesion;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    } 
+    
+    @FXML
+    private void btnIniciarSesionOnAction(ActionEvent event) {
+       try {
+            // Cargo la vista
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Home.fxml"));
+
+            // Cargo el padre
+            Parent root = loader.load();
+
+            // Obtengo el controlador
+            HomeController controlador = loader.getController();
+
+            // Creo la scene y el stage
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            // Asocio el stage con el scene
+            stage.setScene(scene);
+            stage.show();
+
+            // Ciero la ventana donde estoy
+            Stage myStage = (Stage) this.btnIniciarSesion.getScene().getWindow();
+            myStage.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(RegisterController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    public void closeWindows() {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Home.fxml"));
+
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+
+            Stage myStage = (Stage) this.btnIniciarSesion.getScene().getWindow();
+            myStage.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
