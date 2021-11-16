1367f70301a641e5de78289677de0758b6c92496
Admin <luis.pablo.peralta.tapia@gmail.com>
2021-11-15 22:47:04 -0600

    Se crearon los reportes

--- src/Connection/ConnectionMsql.java
+++ src/Connection/ConnectionMsql.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionMsql {
+    
 
     private Connection connection;
     private String user = "root";
@@ -15,6 +16,10 @@ public class ConnectionMsql {
 
     private String url = "jdbc:mysql://" + server + ":" + port + "/" + DbName + "?serverTimezone=UTC";
     private String driver = "com.mysql.cj.jdbc.Driver";
+    
+    public Connection getCon() {
+        return connection;
+    }
     
     public ConnectionMsql() {
 
@@ -31,6 +36,24 @@ public class ConnectionMsql {
             e.printStackTrace();
         }
     }
+    
+    
+    public void ConnectionMsql() {
+
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Conexion Realizada  correctamente");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Ocurrio un error en la conexion");
+            System.err.println("Mensaje del erro:" + e.getMessage());
+            System.err.println("Detalle del error:");
+            e.printStackTrace();
+        }
+    }
+    
     
     public Connection getConnection() {
         return connection;
--- src/Controller/GraficasController.java
+++ src/Controller/GraficasController.java
@@ -1,11 +1,16 @@
 package Controller;
 
 import Dao.GrafiacasDAO_Productos;
+import Connection.ConnectionMsql;
+
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,53 +18,143 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class GraficasController implements Initializable {
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class GraficasController extends ConnectionMsql implements Initializable {
 
     /*Volver*/
     @FXML
     private Button btnVolver;
-    
-    /*Combo Box*/
+
+    /*Reporte*/
     @FXML
-    private ComboBox<GrafiacasDAO_Productos> CMBProductos;
+    private Button btnReporte;
     
-    /*Graficar*/
     @FXML
-    private Button btnGraficar;
-    
+    private Button btnAbrirReporte;
+
+    /*Grafica*/
+    @FXML
+    PieChart graficos;
+    ObservableList<PieChart.Data> pieChartData;
+
     /*Inpust tipo date*/
     @FXML
     private DatePicker dateFechaInicio;
 
     @FXML
     private DatePicker dateFechaFin;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        GrafiacasDAO_Productos d= new GrafiacasDAO_Productos();
-        ObservableList<GrafiacasDAO_Productos> obsDatos = d.getDatos();
-        this.CMBProductos.setItems(obsDatos);
+        cargarDatos();
+        graficos.setData(pieChartData);
+    }
+
+    /*Accion para Generar reportes*/
+    @FXML
+    void btnReporteOnAction(ActionEvent event) throws FileNotFoundException, DocumentException {
+        ResultSet res;
+        try {
+            FileOutputStream archivo = new FileOutputStream("Reporte" + ".pdf");
+            Document documento = new Document();
+            PdfWriter.getInstance(documento, archivo);
+            documento.open();
+
+            Paragraph parrafo = new Paragraph("Reporte de venta\n\n");
+            parrafo.setAlignment(1);
+            documento.add(parrafo);
+            
+            this.ConnectionMsql();
+            String SQL = "SELECT * FROM ventas_p";
+            PreparedStatement sentencia = this.getCon().prepareStatement(SQL);
+            res = sentencia.executeQuery();
+            while (res.next()) {
+                documento.add(new Paragraph("Nombre del cliente:"+res.getString("NombreCliente")+"\n"));
+                documento.add(new Paragraph("RFC:"+res.getString("RFC")+"\n"));
+                documento.add(new Paragraph("Fecha de venta:"+res.getString("Fecha")+"\n"));
+                documento.add(new Paragraph("Producto:"+res.getString("NombreProducto")+"\n"));
+                documento.add(new Paragraph("Unidadades:"+ res.getInt("Cantidad")+"\n"));
+                documento.add(new Paragraph("Precio:"+ res.getInt("Cantidad")+"\n"));
+                documento.add(new Paragraph("Concepto:"+ res.getInt("Cantidad")+"\n"));
+                documento.add(new Paragraph("----------------------------------------------------\n"));
+            }
+            documento.close();
+
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Exito");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Reporte Creado correctamente");
+            alerta.initStyle(StageStyle.UTILITY);
+            alerta.showAndWait();
+
+        } catch (Exception e) {
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Error");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Hubo un error  al crear el reporte");
+            alerta.initStyle(StageStyle.UTILITY);
+            alerta.showAndWait();
+        }
+
     }
     
-    
-    /*Accion para Graficar*/
     @FXML
-    void btnGraficarOnAction(ActionEvent event) {
-        GrafiacasDAO_Productos productos= CMBProductos.getSelectionModel().getSelectedItem();
-        String fechaIni=dateFechaInicio.getValue().toString();
-        String fechaFin=dateFechaFin.getValue().toString();
-        System.out.println("Producto:"+productos+"Fecha de inicio:"+fechaIni+"Fecha fin:"+fechaFin);
+    void btnAbrirReporteOnAction(ActionEvent event) {
+        try {
+            File path=new File("Reporte" + ".pdf");
+            Desktop.getDesktop().open(path);
+        } catch (Exception e) {
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Error al abrir el archivo");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Hubo un error  al abrir el archivo");
+            alerta.initStyle(StageStyle.UTILITY);
+            alerta.showAndWait();
+        }
+    }
+
+    /*Cargar Datos de grafica*/
+    public void cargarDatos() {
+        ResultSet res;
+        pieChartData = FXCollections.observableArrayList();
+        try {
+            this.ConnectionMsql();
+            String SQL = "SELECT * FROM ventas_p";
+            PreparedStatement sentencia = this.getCon().prepareStatement(SQL);
+            res = sentencia.executeQuery();
+            while (res.next()) {
+                pieChartData.add(new PieChart.Data(res.getString("NombreProducto"), res.getInt("Cantidad")));
+            }
+        } catch (Exception e) {
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Error");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Hubo un error consulte con el administrador");
+            alerta.initStyle(StageStyle.UTILITY);
+            alerta.showAndWait();
+        }
     }
 
     /*Botones para Cerrar ventana*/
     @FXML
     private void btnVolverOnAction(ActionEvent event) {
-       try {
+        try {
             // Cargo la vista
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Menu.fxml"));
 
@@ -84,10 +179,10 @@ public class GraficasController implements Initializable {
             myStage.close();
 
         } catch (IOException ex) {
-            Logger.getLogger( GraficasController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GraficasController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void closeWindows() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Menu.fxml"));
@@ -100,8 +195,8 @@ public class GraficasController implements Initializable {
             stage.setScene(scene);
             stage.show();
 
-           Stage myStage = (Stage) this.btnVolver.getScene().getWindow();
-           myStage.close();
+            Stage myStage = (Stage) this.btnVolver.getScene().getWindow();
+            myStage.close();
         } catch (IOException ex) {
             Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
         }
